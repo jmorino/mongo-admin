7b2c49e150a1c9b1bc632227c053f29f5b55d3d9
jmorino <jerome.morino@gmail.com>
2019-11-28 15:09:08 +0100

    enhance results for collection overview

--- src/server/api/use-cases/get-collection-overview.js
+++ src/server/api/use-cases/get-collection-overview.js
@@ -3,9 +3,35 @@
 import { ensureCollection } from '../../db';
 
 
+const fieldSorter = (a,b) => {
+	if (a === '_id') { return -1 }
+	if (b === '_id') { return +1 }
+	if (a < b) { return -1 }
+	if (a > b) { return +1 }
+	return 0;
+};
+
+const findAllKeys = (obj, parentKey = null) => {
+	if (Array.isArray(obj)) {
+		return findAllKeys(obj[0], parentKey);
+	}
+	if (typeof obj === 'object' && obj !== null) {
+		const keys = Object.keys(obj);
+		const nestedKeys = keys.map(key => findAllKeys(obj[key], key));
+		
+		const prefix = parentKey ? `${parentKey}.` : '';
+		return keys.concat(...nestedKeys).map(key => `${prefix}${key}`);
+	}
+	return [];
+};
+
+
 export default async (dbName, collectionName) => {
 	const collection = await ensureCollection(dbName, collectionName);
 	const stats = await collection.stats();
+	const sample = stats.count > 0 ? await collection.findOne() : {};
+	const fields = findAllKeys(sample).sort(fieldSorter);
+	const rootFields = Object.keys(sample).sort(fieldSorter);
 
-	return { database: dbName, name: collectionName, stats };
+	return { database: dbName, name: collectionName, stats, rootFields, fields };
 };
